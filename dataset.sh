eaad3efbb538d5408b42a5a7fee6f4e21f0f9987
Key5n <k.nogami.472@stn.nitech.ac.jp>
2025-09-25 18:23:54 +0900

    initialize a repository

---
+++ dataset.sh
@@ -0,0 +1,14 @@
+# PAF Prediction Challenge Database
+wget -r -N -c -np https://physionet.org/files/afpdb/1.0.0/
+# CU Ventricular Tachyarrrhythmia Database
+wget -r -N -c -np https://physionet.org/files/cudb/1.0.0/
+# MIT-BIH Arrythmia Database
+wget -r -N -c -np https://physionet.org/files/nsr2db/1.0.0/
+# MIT-BIH Atrial Fibrillation Database
+wget -r -N -c -np https://physionet.org/files/afdb/1.0.0/
+# Long Term AF Database
+wget -r -N -c -np https://physionet.org/files/ltafdb/1.0.0/
+# SHDB-AF: a Japanese Holter ECG database of atrial fibrillation
+wget -r -N -c -np https://physionet.org/files/shdb-af/1.0.1/
+# Sudden Cardiac Death Holter Database
+wget -r -N -c -np https://physionet.org/files/sddb/1.0.0/
