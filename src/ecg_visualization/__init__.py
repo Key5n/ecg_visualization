a92c81220586d5c1f4c69e7925cce5a290611dc5
Key5n <key_ngmn@outlook.jp>
2026-03-19 16:12:48 +0900

    fix: delete unnecessary scripts

--- src/ecg_visualization/__init__.py
+++ src/ecg_visualization/__init__.py
@@ -1,14 +1,9 @@
-from ecg_visualization.scripts.ecg_visualization import ecg_visualization
+from ecg_visualization.scripts.entity_info import entity_info
 from ecg_visualization.scripts.study import study_all_entities
 from ecg_visualization.scripts.visualize import visualize_all_studies
-from ecg_visualization.scripts.debug_visualize import run_debug_visualize
-from ecg_visualization.scripts.entity_info import entity_info
-
 
 __all__ = [
-    "ecg_visualization",
     "study_all_entities",
     "visualize_all_studies",
-    "run_debug_visualize",
     "entity_info",
 ]
