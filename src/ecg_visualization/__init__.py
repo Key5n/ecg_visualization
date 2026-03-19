8f53329b21aee756046b6c496facdd69ee346779
Key5n <key_ngmn@outlook.jp>
2026-03-19 18:12:39 +0900

    chore: wire cudb anomaly task

--- src/ecg_visualization/__init__.py
+++ src/ecg_visualization/__init__.py
@@ -1,9 +1,11 @@
 from ecg_visualization.scripts.entity_info import entity_info
 from ecg_visualization.scripts.study import study_all_entities
 from ecg_visualization.scripts.visualize import visualize_all_studies
+from ecg_visualization.scripts.cudb_anomaly_scores import cudb_anomaly_scores
 
 __all__ = [
     "study_all_entities",
     "visualize_all_studies",
     "entity_info",
+    "cudb_anomaly_scores",
 ]
